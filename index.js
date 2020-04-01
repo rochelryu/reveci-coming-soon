1cdc79516512497935f547980bb09b29970614c7
rochelryu <38214577+rochelryu@users.noreply.github.com>
2020-04-01 23:07:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'images')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
